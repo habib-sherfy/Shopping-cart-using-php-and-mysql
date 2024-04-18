d9b39025ca86fc2af2f08e63168b38f3fd127126
habib_sherfy <shafynoor82@gmail.com>
2024-04-18 18:00:55 +0300

    first step

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>shopping cart</title>
+
+    <!-- custom css files -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+
+
+
+
+<!-- custom javascript -->
+<script src="js/script.js"></script>
+</body>
+</html>
