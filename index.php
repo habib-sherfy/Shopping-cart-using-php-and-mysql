246021f5a58a55af24d1931dc9cfd764be6f2803
habib_sherfy <shafynoor82@gmail.com>
2024-04-19 08:38:29 +0300

    added the header section

--- index.php
+++ index.php
@@ -12,7 +12,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    <!-- header section starts here -->
+    <div class="header-body">
     
+    <!-- company logo here -->
+        <a href="index.php">cart trial.co</a>
+
+    <!-- navbar links here     -->
+        <nav class="navbar">
+            <a href="#">Add products</a>
+            <a href="#">View products</a>
+            <a href="#">Show products</a>
+        </nav>
+
+    <!-- cart icons here     -->
+        <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i></a>
+    </div>
 
 
 
